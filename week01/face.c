58b2987304ac20d4561eedf313623997db076578
Pat Chambers <z5264081@cse.unsw.edu.au>
2023-09-13 17:43:31 +1000

    Added code files

---
+++ week01/face.c
@@ -0,0 +1,14 @@
+// Basic Hello World program
+// Pat Chambers (z5264081), 13/9/23
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o \n");
+    printf("\\_/\n");
+
+    return 0;
+}
